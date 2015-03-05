acde843ce195852320c15ca5850a70dc2cfbca2f
Yang <yang_sg12@126.com>
2015-03-05 23:31:15 +0800

    good

---
+++ concorrency_cookbook/synchronization/method/synchronization/block/Bank.java
@@ -0,0 +1,21 @@
+package concorrency_cookbook.synchronization.method.synchronization.block;
+
+import concorrency_cookbook.synchronization.method.synchronization.block.Account;
+
+/**
+ * Created by Yang on 2015/1/30.
+ */
+public class Bank implements Runnable {
+    private Account account;
+    public Bank(Account account){
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i=0;i<100;i++){
+            System.out.printf(" ------------银行扣了 %d 钱了！！！\n",i);
+            account.subAmount(1000);
+        }
+    }
+}
---
+++ concorrency_cookbook/synchronization/method/synchronization/block/Company.java
@@ -0,0 +1,21 @@
+package concorrency_cookbook.synchronization.method.synchronization.block;
+
+import concorrency_cookbook.synchronization.method.synchronization.block.Account;
+
+/**
+ * Created by Yang on 2015/1/30.
+ */
+public class Company implements Runnable {
+    private Account account;// 账户上加钱
+    public Company(Account account){
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i=0;i<100;i++){
+            System.out.printf("++++++公司加 %d钱了！！！\n", i);
+            account.addAmount(1000);
+        }
+    }
+}
